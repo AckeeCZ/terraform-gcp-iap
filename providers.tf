ab6b12fcbd56a032410eac83a411d83a13f10227
Tibor Tribus <tibor@Tibors-MacBook-Air.local>
2021-02-03 20:15:36 +0100

    💥 Update k8s provider cred login
    Related: #50479

--- providers.tf
+++ providers.tf
@@ -3,9 +3,7 @@ provider "kubernetes" {
 
   load_config_file = false
   host             = "https://${var.cluster_endpoint}"
-
-  username = var.cluster_user
-  password = var.cluster_pass
+  token            = var.cluster_token
 
   cluster_ca_certificate = var.cluster_ca_certificate
 }
